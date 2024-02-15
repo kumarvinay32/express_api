2f32d5ea958b5ee40635c6ccd905d81e2cd74fea
Vinay Kumar <vinay@zimyo.com>
2024-02-15 10:23:31 +0530

    package artecture changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+/*!
+ * express api
+ * Copyright(c) 2024-2025 Vinay Kumar
+ * MIT Licensed
+ */
+
+'use strict';
+
 module.exports = require('./lib');
--- install/index.js
+++ install/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const DEFAULT_DATA = require("./constants");
-const rootDirectory = path.resolve(__dirname, '../../../');
+const rootDirectory = path.resolve(__dirname, '../../../../');
 const envFilePath = path.join(rootDirectory, '.env');
 
 const checkCreareDir = (directory) => {
@@ -30,12 +30,12 @@ const checkWriteText = (directory, text) => {
         fs.writeFileSync(directory, text);
     }
 }
-checkWriteText(envFilePath, 'NODE_ENV=dev\nSRC=""');
+checkWriteText(envFilePath, `NODE_ENV=dev\nSRC=src`);
 require("dotenv").config({ path: envFilePath });
 
 (async () => {
     try {
-        const _src_dir = path.join(rootDirectory, process.env.SRC || "/");
+        const _src_dir = path.join(rootDirectory, process.env.SRC || "src");
         checkCreareDir(_src_dir);
         checkCreareDir(path.join(_src_dir, 'config'));
         checkCreareDir(path.join(_src_dir, 'models'));
@@ -51,9 +51,14 @@ require("dotenv").config({ path: envFilePath });
         checkCrearefile(path.join(_src_dir, 'routes'), '');
         checkCrearefile(path.join(_src_dir, 'helpers'), '');
         checkCrearefile(path.join(_src_dir, 'activities'), '');
+        checkCrearefile(path.join(_src_dir, 'models'), '');
         checkWriteText(path.join(_src_dir, 'models', 'datasource', 'Database Named Folder'), 'Create Database named folder as per DB Config.');
-        checkWriteText(path.join(_src_dir, '.gitignore'), Buffer.from(`IyBVc2VyIHNwZWNpZmljICYgYXV0b21hdGljYWxseSBnZW5lcmF0ZWQgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCm5vZGVfbW9kdWxlcwpwYWNrYWdlLWxvY2suanNvbgouZW52Ci5lbnYuKgoqL2xvZ3MvKi4qCiovbG9ncy8qLyouKgoqLm1vCioub2xkCioucGRmCioueGxzCioueGxzeAoqLnppcAoqL2NvbmZpZy8qLioKISovY29uZmlnL2FwcENvbmZpZy5qcwplY29zeXN0ZW0uY29uZmlnLmpzCiMgSURFIGFuZCBlZGl0b3Igc3BlY2lmaWMgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKL25icHJvamVjdAouaWRlYQoudnNjb2RlCi8ucHJvamVjdAovLmJ1aWxkcGF0aAovLnNldHRpbmdzLwoKIyBPUyBnZW5lcmF0ZWQgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjCi5EU19TdG9yZQouRFNfU3RvcmU/Ci5fKgouU3BvdGxpZ2h0LVYxMDAKLlRyYXNoZXMKSWNvbj8KZWh0aHVtYnMuZGIKVGh1bWJzLmRiCg==`, 'base64').toString());
-        checkWriteText(path.join(_src_dir, 'readme.md'), Buffer.from(`IyMgQWJvdXQKClRoaXMgaXMgYSBzaW1wbGUgY3VzdG9taXplZCBOT0RFLUpTIHNldHVwIGZvciBkZXZlbG9waW5nIEFQSXMKCiMjIEdsb2JhbCBFbnZpcm9ubWVudAoKLSAqKiBOb2RlIEpTOiAxNC4xNS4wIC0gKiogW0luc3RhbGwgTm9kZSBKc10oaHR0cHM6Ly9ub2RlanMub3JnL2VuL2Rvd25sb2FkLykqKiAqKgotICoqIE5vZGVtb246IDIuMC42ICoqCi0gKiogU2VxdWVsaXplIENMSTogNi4yLjAgLSAqKiBbU2VxdWVsaXplIFJlZmVyZW5jZV0oaHR0cHM6Ly9zZXF1ZWxpemUub3JnL21hc3Rlci8pKiogKioK`, 'base64').toString());
+        checkWriteText(path.join(rootDirectory, '.gitignore'), Buffer.from(`IyBVc2VyIHNwZWNpZmljICYgYXV0b21hdGljYWxseSBnZW5lcmF0ZWQgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCm5vZGVfbW9kdWxlcwpwYWNrYWdlLWxvY2suanNvbgouZW52Ci5lbnYuKgoqL2xvZ3MvKi4qCiovbG9ncy8qLyouKgoqLm1vCioub2xkCioucGRmCioueGxzCioueGxzeAoqLnppcAoqL2NvbmZpZy8qLioKISovY29uZmlnL2FwcENvbmZpZy5qcwplY29zeXN0ZW0uY29uZmlnLmpzCiMgSURFIGFuZCBlZGl0b3Igc3BlY2lmaWMgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKL25icHJvamVjdAouaWRlYQoudnNjb2RlCi8ucHJvamVjdAovLmJ1aWxkcGF0aAovLnNldHRpbmdzLwoKIyBPUyBnZW5lcmF0ZWQgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjCi5EU19TdG9yZQouRFNfU3RvcmU/Ci5fKgouU3BvdGxpZ2h0LVYxMDAKLlRyYXNoZXMKSWNvbj8KZWh0aHVtYnMuZGIKVGh1bWJzLmRiCg==`, 'base64').toString());
+        checkWriteText(path.join(rootDirectory, 'readme.md'), Buffer.from(`IyMgQWJvdXQKClRoaXMgaXMgYSBzaW1wbGUgY3VzdG9taXplZCBOT0RFLUpTIHNldHVwIGZvciBkZXZlbG9waW5nIEFQSXMKCiMjIEdsb2JhbCBFbnZpcm9ubWVudAoKLSAqKiBOb2RlIEpTOiAxNC4xNS4wIC0gKiogW0luc3RhbGwgTm9kZSBKc10oaHR0cHM6Ly9ub2RlanMub3JnL2VuL2Rvd25sb2FkLykqKiAqKgotICoqIE5vZGVtb246IDIuMC42ICoqCi0gKiogU2VxdWVsaXplIENMSTogNi4yLjAgLSAqKiBbU2VxdWVsaXplIFJlZmVyZW5jZV0oaHR0cHM6Ly9zZXF1ZWxpemUub3JnL21hc3Rlci8pKiogKioK`, 'base64').toString());
+        checkCrearefile(rootDirectory, 'server');
+        let packageJs = JSON.parse(fs.readFileSync(path.join(rootDirectory, 'package.json')).toString());
+        packageJs.main = 'server.js';
+        fs.writeFileSync(path.join(rootDirectory, 'package.json'), JSON.stringify(packageJs, null, 2));
     } catch (err) {
         console.log(err);
     }
